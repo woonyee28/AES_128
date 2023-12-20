8d2a98fb376aea599eb361116156e45a4fb8e91b
Woon Yee Ng <ngnwy289@gmail.com>
2023-12-20 21:24:08 +0800

    all good now but the input must be 32 char hexadecimal for AES128

--- functions.py
+++ functions.py
@@ -112,7 +112,6 @@ class AES_128():
         keys = self.keyScheduling(key)    
         state = self.add_round_key(state, keys[0])
         
-
         for r in range(1,10):  
             state = self.subBytes(state)
             state = self.shiftRows(state)
@@ -124,21 +123,115 @@ class AES_128():
         state = self.shiftRows(state)
         state = self.add_round_key(state, keys[10])
         return ''.join(''.join(row) for row in state)
+    
+    inv_Sbox = (
+        0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb, 
+        0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb, 
+        0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e, 
+        0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25, 
+        0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92, 
+        0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84, 
+        0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06, 
+        0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b, 
+        0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73, 
+        0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e, 
+        0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b, 
+        0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4, 
+        0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f, 
+        0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef, 
+        0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61, 
+        0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d
+    )
+
+    def inverse_subBytes(self, state):
+        return [[hex(AES_128.inv_Sbox[int(j, 16)])[2:].zfill(2) for j in row] for row in state]
+
+    def inverse_shiftRows(self, state):
+        state = list(map(list, zip(*state)))
+        
+        state = [state[i][-i:] + state[i][:-i] for i in range(4)]
+
+        state = list(map(list, zip(*state)))
+        return state
+
+    def inverse_mixColumn(self, state):
+        state = list(map(list, zip(*state)))
+        s = [[int(j, 16) for j in row] for row in state]
+        for c in range(4):
+            s0 = self.GMul(0x0e, s[0][c]) ^ self.GMul(0x0b, s[1][c]) ^ self.GMul(0x0d, s[2][c]) ^ self.GMul(0x09, s[3][c])
+            s1 = self.GMul(0x09, s[0][c]) ^ self.GMul(0x0e, s[1][c]) ^ self.GMul(0x0b, s[2][c]) ^ self.GMul(0x0d, s[3][c])
+            s2 = self.GMul(0x0d, s[0][c]) ^ self.GMul(0x09, s[1][c]) ^ self.GMul(0x0e, s[2][c]) ^ self.GMul(0x0b, s[3][c])
+            s3 = self.GMul(0x0b, s[0][c]) ^ self.GMul(0x0d, s[1][c]) ^ self.GMul(0x09, s[2][c]) ^ self.GMul(0x0e, s[3][c])
+
+            s[0][c] = hex(s0)[2:].zfill(2)
+            s[1][c] = hex(s1)[2:].zfill(2)
+            s[2][c] = hex(s2)[2:].zfill(2)
+            s[3][c] = hex(s3)[2:].zfill(2)
+        s = list(map(list, zip(*s)))
+        return s
+    
+    def decrypt(self, ciphertext, key):
+        # Assuming you have a function to generate round keys
+        key = [[int(key[i:i+2], 16) for i in range(j, j+8, 2)] for j in range(0, 32, 8)]
+
+        round_keys = self.keyScheduling(key)
+
+        # Convert ciphertext to state matrix
+        state = [[int(ciphertext[i:i+2], 16) for i in range(j, j+8, 2)] for j in range(0, 32, 8)]
+
+        state = self.add_round_key(state, round_keys[-1])
+
+        # 9 rounds of decryption
+        for i in range(9, 0, -1):
+            state = self.inverse_shiftRows(state)
+            state = self.inverse_subBytes(state)
+            state = self.add_round_key(state, round_keys[i])
+            state = self.inverse_mixColumn(state)
+
+        # Final round of decryption
+        state = self.inverse_shiftRows(state)
+        state = self.inverse_subBytes(state)
+        state = self.add_round_key(state, round_keys[0])
+
+        # Convert state matrix to plaintext
+        return ''.join(''.join(row) for row in state)
+
 
 if __name__ == "__main__":
     aes = AES_128()
-    key = '2b7e151628aed2a6abf7158809cf4f3c'
-    plaintext = '3243f6a8885a308d313198a2e0370734'
-    expected_ciphertext = '3925841d02dc09fbdc118597196a0b32'
 
-    # Encrypt the plaintext and check if it matches the expected ciphertext
-    ciphertext = aes.encrypt(plaintext, key)
-    if ciphertext == expected_ciphertext:
-        print("it works!")
+    # key = '2b7e151628aed2a6abf7158809cf4f3c'
+    # plaintext = '3243f6a8885a308d313198a2e0370734'
+    # expected_ciphertext = '3925841d02dc09fbdc118597196a0b32'
+
+    key = input("Enter a 128-bit hexadecimal (32 characters) KEY for encryption/decryption: ")
+    while len(key) != 32:
+        print("Key must be 128-bit (32 characters) hexadecimal long.")
+        key = input("Enter a 128-bit (32 characters) hexadecimal for encryption/decryption: ")
+
+
+    choice = input("Do you want to (E)ncrypt or (D)ecrypt? ")
+    while choice not in ['E', 'D']:
+        print("Invalid choice. Please enter E for Encrypt or D for Decrypt.")
+        choice = input("Do you want to (E)ncrypt or (D)ecrypt? ")
+
+    if choice == 'E':
+        plaintext = input("Enter a 128-bit (32 characters) hexadecimal for encryption: ")
+        while len(plaintext) != 32:
+            print("Plaintext must be 128-bit (32 characters) hexadecimal long.")
+            plaintext = input("Enter a 128-bit (32 characters) hexadecimal plaintext for encryption: ")
+
+        # Encrypt the plaintext
+        ciphertext = aes.encrypt(plaintext, key)
+        print("Encrypted text is: ", ciphertext)
+
     else:
-        print("no")
-        print(ciphertext)
-        print(expected_ciphertext)
+        ciphertext = input("Enter a 128-bit (32 characters) hexadecimal ciphertext for decryption: ")
+        while len(ciphertext) != 32:
+            print("Ciphertext must be 128-bit (32 characters) hexadecimal long.")
+            ciphertext = input("Enter a 128-bit (32 characters) hexadecimal ciphertext for decryption: ")
 
+        # Decrypt the ciphertext
+        dec_plaintext = aes.decrypt(ciphertext, key)
 
-# https://www.cryptool.org/en/cto/aes-step-by-step
+        print("Decrypted text is: ", dec_plaintext)
